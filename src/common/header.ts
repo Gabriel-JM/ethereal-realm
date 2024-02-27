5279f90e1fc4880b236581891368ae2981a1ff07
Gabriel José <gabriel.josemelo@hotmail.com>
2024-02-27 20:48:16 -0300

    feat: change fonts used

--- src/common/header.ts
+++ src/common/header.ts
@@ -5,6 +5,7 @@ const headerStyles = css`
     padding: 8px 12px;
     background-color: var(--black-90);
     border-bottom: 1px solid var(--black-75);
+    font-family: Caudex, sans-serif;
   }
 `
 
