c7e730ced0abeb1c2aa1caa8c98eed888221892a
Gabriel José <gabriel.josemelo@hotmail.com>
2024-04-09 16:10:32 -0300

    feat: improve weapon doc card style

--- src/equipments/weapons/doc/card/weapon-doc-card.ts
+++ src/equipments/weapons/doc/card/weapon-doc-card.ts
@@ -7,48 +7,52 @@ type WeaponDocCardProps = Weapon
 export function weaponDocCard(props: WeaponDocCardProps) {
   return html`
     <div class="weapon-doc-card">
-      <div>
+      <div class="header">
         <div>
-          <h4>${props.name}</h4>
-          <p>${props.rarity}</p>
+          <h4 class="name">${props.name}</h4>
+          <p>${props.rarity} ${props.type}</p>
         </div>
         <div>
-          <img src="" alt="🗡️" />
+          <img class="icon" src="" alt="🗡️" />
         </div>
       </div>
-      <div>
-        <p>Dano</p>
+      
+      <div class="prop-container">
+        <p class="prop-name">Dano</p>
         ${props.damage.map(dmg => {
           return html`
             <p>${dmg.value} (${dmg.types.join(', ')})</p>
           `
         })}
       </div>
-      <div>
-        <p>Atributos</p>
+      
+      <div class="prop-container">
+        <p class="prop-name">Atributos</p>
         <p>${props.attributes.join(', ')}</p>
       </div>
-      <div>
-        <p>Modificador de Iniciativa</p>
-        <p>
-          ${props.initiativeModifier > 0
-            ? `+${props.initiativeModifier}`
-            : props.initiativeModifier.toString()
-          }
-        </p>
-      </div>
-      <div>
+      
+      <div class="prop-container group">
         <div>
-          <p>Distância</p>
+          <p class="prop-name">Iniciativa</p>
+          <p>
+            ${props.initiativeModifier > 0
+              ? `+${props.initiativeModifier}`
+              : props.initiativeModifier.toString()
+            }
+          </p>
+        </div>
+        <div>
+          <p class="prop-name">Distância</p>
           <p>${props.distance ?? '-'}</p>
         </div>
         <div>
-          <p>Preço</p>
+          <p class="prop-name">Preço</p>
           <p>${props.price}</p>
         </div>
       </div>
+
       <div>
-        <p>Descrição</p>
+        <p class="prop-name">Descrição</p>
         <p>${props.description}</p>
       </div>
     </div>
