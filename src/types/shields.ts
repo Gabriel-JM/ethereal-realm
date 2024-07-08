eb589a359e79ba32223e70d18af8932bd9bf5f0d
Gabriel José <gabriel.josemelo@hotmail.com>
2024-07-08 16:16:52 -0300

    feat: add shield related types

---
+++ src/types/shields.ts
@@ -0,0 +1,21 @@
+import { ItemRarity } from './weapons'
+
+export type Shield = {
+  id: `shld_${string}`
+  name: string
+  rarity: ItemRarity
+  type: ShieldTypes
+  attributes: ShieldAttributes[]
+  price: number
+  damageReduction: string
+  description: string
+}
+
+export type ShieldTypes = (
+  'buckler'
+  |'lightShield'
+  |'midShield'
+  |'heavyShield'
+)
+
+export type ShieldAttributes = 'light' | 'heavy'
